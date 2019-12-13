94f547ff4955db12c69baa82e5e551c4e1a6a99d
Koen Van der Plas <koen.vanderplas@student.romerocollege.be>
2019-12-13 09:56:45 +0100

    opgave

---
+++ Repos_Examen2019/Statistiek/Statistiek/Program.cs
@@ -0,0 +1,16 @@
+﻿// 5ITN11 - 2019-12-13
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Statistiek
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
